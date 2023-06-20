3c603e3976c03feb30417823e949d1f91fdee966
shubhangNarain <snvishere@gmail.com>
2023-06-20 12:52:54 +0530

    done with final changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,43 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers, increment } from '@/slices/userSlice';
+import { AppDispatch, RootState } from '@/store/store';
+
+
 
 export default function Home() {
+
+  const userRef = useRef(false);
+
+  const { entities, loading, value } = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch<AppDispatch>();
+
+  console.log('loading', loading)
+  // console.log(entities);
+
+  useEffect(() =>{
+    if(userRef.current === false){
+      dispatch(fetchUsers());
+    }
+    return () =>{
+      userRef.current = true;
+    }
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className=''>
+      {loading? (
+        <h1>Loading...</h1>
+      ) : (
+        entities?.map((user: any) => <h3 key={user.id}>{user.name}</h3> )
+      )}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      {value}
+    </div>
   )
 }
--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 
 export const fetchUsers = createAsyncThunk('users/getAllUsers', async (thunkApi) =>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=5')
+    const res = await fetch('https://jsonplaceholder.typicode.com/users?_limit=10')
     const data = await res.json()
     return data
 })
@@ -10,17 +10,30 @@ export const fetchUsers = createAsyncThunk('users/getAllUsers', async (thunkApi)
 
 const initialState = {
     entities: [],
+    loading : false,
+    value: 10
 } as any
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {},
+    reducers: {
+        increment: (state) => {
+            state.value++
+        },
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchUsers.fulfilled, (state, action) => {
             state.entities.push(...action.payload)
+            state.loading = false
+        })
+
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.loading = true
         })
     }
 })
+
+export const { increment } = userSlice.actions
 
 export default userSlice.reducer
